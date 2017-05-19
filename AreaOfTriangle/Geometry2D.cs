a03bd99fab19141fb3f9281e12278ed1b1eb437c
Nazar <dutka.nazar@gmail.com>
2017-05-19 19:10:35 +0300

    do

--- AreaOfTriangle/Geometry2D.cs
+++ AreaOfTriangle/Geometry2D.cs
@@ -4,29 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AreaOfTriangle
+namespace Lib
 {
-    class Program
+    interface ITask //bad name but whatever
+    {
+        double Area(double[] x, double[] y);
+    }
+
+    public class Geometry2D:ITask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing methods");
+
             double[] A = new double[] { 0, 0, 6 };
             double[] B = new double[] { 0, 6, 0 };
-            Program p = new Program();
-            try
-            {
-                double S = p.FindArea(A, B);
-                Console.WriteLine($"S={S}");
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.ReadLine();
+
         }
-        public double FindArea(double[] x, double[] y)
+        public double FindTiangleArea(double[] x, double[] y)
         {
+            if (x==null && y== null)
+            {
+                throw new ArgumentNullException();
+            }
             if (x.Length!=3 || y.Length!=3)
             {
                 throw new ArgumentException($"Argument must contain coordinates of 3 points. Received x.Length= {x.Length}, y.Length= {y.Length}");
@@ -41,6 +40,26 @@ namespace AreaOfTriangle
         private double FindLineLegth(double x1, double x2, double y1, double y2)
         {
             return Math.Sqrt(Math.Pow((x1 - x2), 2d) + Math.Pow((y1 - y2), 2d));           
+        }
+        public double FindPolygonArea(double[] x, double[] y)
+        {
+            if (x == null && y == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            //todo etc.
+
+            switch (switch_on)
+            {
+                default:
+            }
+
+        }
+
+        public double Area(double[] x, double[] y)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ AreaOfTriangleTests/AreaTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AreaOfTriangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle.Tests
+{
+    [TestClass()]
+    public class AreaTests
+    {
+        [TestMethod()]
+        public void FindLineLegthTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
