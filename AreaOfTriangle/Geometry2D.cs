2bfe16395c17bb583b7fd33449d046434bd772f6
Nazar <dutka.nazar@gmail.com>
2017-05-19 19:48:37 +0300

    ++

--- AreaOfTriangle/Geometry2D.cs
+++ AreaOfTriangle/Geometry2D.cs
@@ -10,7 +10,11 @@ namespace Lib
     {
         double Area(double[] x, double[] y);
     }
-
+    public struct Point2D 
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
     public class Geometry2D:ITask
     {
         static void Main(string[] args)
@@ -20,19 +24,19 @@ namespace Lib
             double[] B = new double[] { 0, 6, 0 };
 
         }
-        public double FindTiangleArea(double[] x, double[] y)
+        public double FindTiangleArea(Point2D[] A)
         {
-            if (x==null && y== null)
+            if (A==null)
             {
                 throw new ArgumentNullException();
             }
-            if (x.Length!=3 || y.Length!=3)
+            if (A.Length!=3)
             {
-                throw new ArgumentException($"Argument must contain coordinates of 3 points. Received x.Length= {x.Length}, y.Length= {y.Length}");
+                throw new ArgumentException($"Argument must contain coordinates of 3 points. Received {A.Length} points");
             }
-            double LengthA = FindLineLegth(x[0], x[1], y[0], y[1]);
-            double LengthB = FindLineLegth(x[1], x[2], y[1], y[2]);
-            double LengthC = FindLineLegth(x[2], x[0], y[2], y[0]);
+            double LengthA = FindLineLegth(A[0].X, A[1].X, A[0].Y, A[1].Y);
+            double LengthB = FindLineLegth(A[1].X, A[2].X, A[1].Y, A[2].Y);
+            double LengthC = FindLineLegth(A[2].X, A[0].X, A[2].Y, A[0].Y);
             double p = (LengthA + LengthB + LengthC) / 2d;
             double S = Math.Sqrt(p * (p - LengthA) * (p - LengthB) * (p - LengthC));
             return S;
@@ -41,7 +45,7 @@ namespace Lib
         {
             return Math.Sqrt(Math.Pow((x1 - x2), 2d) + Math.Pow((y1 - y2), 2d));           
         }
-        public double FindPolygonArea(double[] x, double[] y)
+        public double FindPolygonArea(double[] points)
         {
             if (points == null)
             {
